c055fec6935e5d7b263892fd83e285e049bed4ce
BrettFoster07 <113189795+BrettFoster07@users.noreply.github.com>
2022-12-16 01:15:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+userinput=input("Please Input A Word")
+negative=-1
+ran=""
+print()
+for i in range(0,len(userinput)):
+  negative=negative+1
+  ran=ran+userinput[negative]
+  print(ran)
+  task=0
+print()
